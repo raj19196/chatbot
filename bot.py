aa700e3b487df1848ab4dc5911cf636703106824
raj19196 <raj19196@gmail.com>
2018-12-19 18:24:12 -0500

    updtae

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ def receive_message():
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
-                    response_sent_text = message['message'].get('text')
+                    response_sent_text = message['message'].get('text') + "Its chat bot "
                     send_message(recipient_id, response_sent_text)
                 #if user sends us a GIF, photo,video, or any other non-text item
                 if message['message'].get('attachments'):
